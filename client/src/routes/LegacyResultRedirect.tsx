02b0b075b5433b0cbdd6550c05a5e819d359e726
duckmaster1461 <144226087+duckmaster1461@users.noreply.github.com>
2026-03-13 01:33:09 +0530

    Delete client directory

--- client/src/routes/LegacyResultRedirect.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams, Navigate } from 'react-router-dom';
-
-export default function LegacyResultRedirect() {
-  const { slug } = useParams();
-  return <Navigate to={slug ? `/results/${slug}` : '/results'} replace />;
-}
