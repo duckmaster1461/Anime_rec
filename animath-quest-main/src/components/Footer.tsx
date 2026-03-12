c4aa4ccd45d4dc05b0afd140968b2d93ae13ca38
duckmaster1461 <144226087+duckmaster1461@users.noreply.github.com>
2026-03-13 01:32:54 +0530

    Delete animath-quest-main directory

--- animath-quest-main/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-brand-dark text-white py-6 px-6 text-center">
-      <p className="text-sm">© 2025 AniMatch. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
